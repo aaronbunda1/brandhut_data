28ddca248bf2059cba1f4c6379e84605e93a8080
cash-abunda <abunda@squareup.com>
2023-11-08 09:34:22 -0500

    test

--- bh_dbt/models/core/ratings.sql
+++ bh_dbt/models/core/ratings.sql
@@ -6,4 +6,4 @@ left join {{var('readable')['hawkspace']}}.reports.report_product_latest_version
     using(product_key)
 left join (select distinct channel_product_id,sku from {{ref('product_pl')}}) pl
     on pl.channel_product_id = p.channel_product_id
-    
+
