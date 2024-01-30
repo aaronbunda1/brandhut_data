c102f60636e4721a8fc41cda16477c02c47202cf
aaronbunda1 <aaronbunda@gmail.com>
2024-01-30 11:48:56 -0500

    Update category.sql
    Fix SKU category

--- bh_dbt/models/core/category.sql
+++ bh_dbt/models/core/category.sql
@@ -1,16 +1,16 @@
 with onanoff as (
 select distinct product_key
 from {{var('readable')['hawkspace']}}.REFERENTIAL.REFERENTIAL_PROJECT
-where project_name IN ('Onanoff','Spot','Cellini')
+where project_name ilike any ('%Onanoff%','%Spot%','%Cellini%')
 )
 
 , datahawk_categories as (select distinct
-p.brand, p.channel_product_id, project_name as category
+p.brand, p.channel_product_id, 
+replace(project_name,' (from projects)','') as category
 from {{var('readable')['hawkspace']}}.REFERENTIAL.REFERENTIAL_PROJECT
 join onanoff using(product_key)
 join {{var('readable')['hawkspace']}}.reports.report_product_latest_version p using(product_key) 
-where NOT project_name IN ('Onanoff','Spot','Cellini')
-
+where NOT project_name ilike any ('%Onanoff%','%Spot%','%Cellini%')
 )
 
 , with_manual as (select *
