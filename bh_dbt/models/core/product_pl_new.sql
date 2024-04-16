7eb50e03b1eed07b8ac9866ba1d30f2855be3a66
aaronbunda@gmail.com <aaronbunda@gmail.com>
2024-04-16 14:04:55 +0000

    add marketplace key to ad spend logic

--- bh_dbt/models/core/product_pl_new.sql
+++ bh_dbt/models/core/product_pl_new.sql
@@ -24,10 +24,7 @@ with non_sp_sd as (
     select 
     date_day,
     channel_product_id,
-<<<<<<< HEAD
     marketplace_key,
-=======
->>>>>>> 7e91c70811214853bf6dae81aeb16178324a2be1
     coalesce(sum("'SponsoredProducts'"),0) as SponsoredProductsCost,
     coalesce(sum("'SponsoredDisplay'"),0) as SponsoredDisplayCost
     from datahawk_share_83514.advertising.advertising_product_campaign_metrics
@@ -194,17 +191,10 @@ coalesce(o.product_samples,0) as manual_product_samples,
 coalesce(o.miscellaneous,0) as manual_miscellaneous_cost,
 coalesce(o.true_up_invoiced,0) as true_up_invoiced,
 coalesce(-pl.net_units_sold*cogs.productcost,pl.COGS) as MANUAL_ONANOFF_COGS,
--- l.SPONSORED_PRODUCTS_COST/2 as SPONSORED_PRODUCTS_COST,
-<<<<<<< HEAD
 sp_sd.sponsoredproductscost/count(*) over (partition by l.posted_local_date,l.asin,l.marketplace_key) as SPONSORED_PRODUCTS_COST,
 sp_sd.sponsoreddisplaycost/count(*) over (partition by l.posted_local_date,l.asin,l.marketplace_key) as sponsored_display_cost,
-=======
-sp_sd.sponsoredproductscost/count(*) over (partition by l.posted_local_date,l.asin) as SPONSORED_PRODUCTS_COST,
-sp_sd.sponsoreddisplaycost/count(*) over (partition by l.posted_local_date,l.asin) as sponsored_display_cost,
->>>>>>> 7e91c70811214853bf6dae81aeb16178324a2be1
 -coalesce(-non_sp_sd.sponsoredbrands/count(*) over (partition by l.posted_local_date,l.account_key,l.marketplace_key,l.brand),0) as DIST_SPONSORED_BRANDS_COST,
 -coalesce(-non_sp_sd.sponsoredbrandsvideo/count(*) over (partition by l.posted_local_date,l.account_key,l.marketplace_key,l.brand),0) as DIST_SPONSORED_BRANDS_VIDEO_COST,
--- coalesce(-non_sp_sd.sponsoreddisplay/count(*) over (partition by l.posted_local_date,l.account_key,l.marketplace_key,l.brand),0) as DIST_SPONSORED_DISPLAY_COST,
 from {{ref('finance_pl_pivot_new')}} l
 left join datahawk_share_83514.referential.referential_currency_rate cr on l.posted_local_date = cr.date_day and l.currency  = cr.currency
 left join {{ref('category')}} c
