8f72c80df5ce3d268d78465659b980c2194df79f
aaronbunda@gmail.com <aaronbunda@gmail.com>
2024-04-17 14:55:04 +0000

    fix errors

--- bh_dbt/models/core/variance.sql
+++ bh_dbt/models/core/variance.sql
@@ -22,37 +22,3 @@ with p as (
 )
 
 select * from p
-
-;
-,datespine as (
-    select 
-        d.date as dbt_updated_at_p1,
-        d2.date as dbt_updated_at_p2
-    from (select distinct date(dbt_updated_at) date from p order by 1) d
-    left join (select distinct date(dbt_updated_at) date from p order by 1) d2
-
-)
-
-,final as (
-select 
-coalesce(p1.metric_group_1,p2.metric_group_1) as metric_group_1,
-coalesce(p1.metric_group_2,p2.metric_group_2) as metric_group_2,
-coalesce(p1.metric_name,p2.metric_name) as metric_name,
-coalesce(p1.brand,p2.brand) as brand,
-coalesce(p1.month,p2.month) as month,
-ds.dbt_updated_at_p1,
-ds.dbt_updated_at_p2,
-coalesce(p1.amount,0) as amount_p1,
-coalesce(p2.amount,0) as amount_p2,
-amount_p2-amount_p1 as delta
-from datespine ds
-left join p p1
-    on p1.dbt_updated_at::DATE = ds.dbt_updated_at_p1
-left join p p2
-    on p1.dbt_updated_at::DATE = ds.dbt_updated_at_p2
-order by 
-    ds.dbt_updated_at_p1,
-    ds.dbt_updated_at_p2
-)
-
-select * from final 
