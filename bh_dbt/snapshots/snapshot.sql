12c9b087150814b86ead469f5d7a976a834cce33
aaronbunda@gmail.com <aaronbunda@gmail.com>
2024-05-15 14:59:40 +0000

    cancel old snapshot

--- bh_dbt/snapshots/snapshot.sql
+++ bh_dbt/snapshots/snapshot.sql
@@ -1,19 +1,4 @@
-{% snapshot product_pl_snapshot %}
 
-{{
-    config(
-      target_database='datahawk_writable_83514',
-      target_schema='snapshots',
-      unique_key='key',
-
-      strategy='timestamp',
-      updated_at='updated_at'
-    )
-}}
-
-select * from datahawk_writable_83514.brandhut.product_pl
-
-{% endsnapshot %}
 
 {% snapshot product_pl_new_snapshot %}
 {{
