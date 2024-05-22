dcfc483c7485fd1258f4251365842e2248896629
aaronbunda1 <aaronbunda@gmail.com>
2024-05-22 11:43:17 -0700

    Aaronbunda1 patch 8 (#47)
    * fix key
    
    * fix key
    
    * filter out nonzero
    
    * fix key again
    
    * fix categories for zens

--- bh_dbt/models/core/category.sql
+++ bh_dbt/models/core/category.sql
@@ -5,5 +5,22 @@ replace(project_name,' (from projects)','') as category,
 creation_date
 from datahawk_share_83514.REFERENTIAL.REFERENTIAL_PROJECT
 where channel_product_id is not null and project_name not IN  
-('Onanoff (from projects)','BuddyPhones','Cellini (from projects)','Zens','Zens Other','cellini','Onanoff','Spot','spot.','Storyphones','Fokus','Pablo Artists'' Choice')
+('Onanoff (from projects)','BuddyPhones','Cellini (from projects)','Zens','Zens Other','cellini','Onanoff','Spot','spot.','Storyphones','Fokus','Pablo Artists'' Choice',
+'Zens MagSafe')
+
+
+union all
+
+select 
+distinct asin as channel_product_id, 
+category as project_name,
+category,
+last_updated::datetime as creation_date
+from datahawk_writable_83514.brandhut.manual_product_categories
+left join (
+select distinct sku, asin 
+from datahawk_share_83514.custom_83514.finance_profit_ledger l 
+where asin is not null
+) using(sku)
+
 order by channel_product_id,creation_date
