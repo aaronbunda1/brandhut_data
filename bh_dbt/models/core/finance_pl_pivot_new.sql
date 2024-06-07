310bae2f0ed559b43665fdbb6f15ecc850a0c380
aaronbunda1 <aaronbunda@gmail.com>
2024-06-07 14:07:59 -0400

    Updates 20240503 (#49)
    * update seed
    
    * update zens categories
    
    * add uncomingled skus map
    
    * update unallocated cost logic

--- bh_dbt/models/core/finance_pl_pivot_new.sql
+++ bh_dbt/models/core/finance_pl_pivot_new.sql
@@ -1,7 +1,8 @@
 select 
 l.*,
-coalesce(b.brand,{{get_brand_from_sku('l.sku')}}) as brand
+coalesce(us.brand,coalesce(b.brand,{{get_brand_from_sku('l.sku')}})) as brand
 from {{ref('finance_product_profit_dh')}} l
 left join {{ref('brand_asin')}} b
     on b.channel_product_id = l.asin
     and b.marketplace_key = l.marketplace_key
+left join {{ref('uncommingled_skus')}} us on l.sku = us.sku
