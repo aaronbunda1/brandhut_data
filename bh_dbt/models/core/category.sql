c629e63157fadca9d0d77c208f14eba8c97aefd9
cash-abunda <120753459+cash-abunda@users.noreply.github.com>
2024-07-18 15:40:50 -0400

    Update category.sql

--- bh_dbt/models/core/category.sql
+++ bh_dbt/models/core/category.sql
@@ -1,26 +1,30 @@
-select 
-channel_product_id,
-project_name,
-replace(project_name,' (from projects)','') as category,
-creation_date
-from datahawk_share_83514.REFERENTIAL.REFERENTIAL_PROJECT
-where channel_product_id is not null and project_name not IN  
-('Onanoff (from projects)','BuddyPhones','Cellini (from projects)','Zens','Zens Other','cellini','Onanoff','Spot','spot.','Storyphones','Fokus','Pablo Artists'' Choice',
-'Zens MagSafe')
+with pre_dedupe as (
+  select 
+  channel_product_id,
+  project_name,
+  replace(project_name,' (from projects)','') as category,
+  creation_date
+  from datahawk_share_83514.REFERENTIAL.REFERENTIAL_PROJECT
+  where channel_product_id is not null and project_name not IN  
+  ('Onanoff (from projects)','BuddyPhones','Cellini (from projects)','Zens','Zens Other','cellini','Onanoff','Spot','spot.','Storyphones','Fokus','Pablo Artists'' Choice',
+  'Zens MagSafe')
+  
+  
+  union all
+  
+  select 
+  distinct asin as channel_product_id, 
+  category as project_name,
+  category,
+  last_updated::datetime as creation_date
+  from datahawk_writable_83514.brandhut.manual_product_categories
+  left join (
+  select distinct sku, asin 
+  from datahawk_share_83514.custom_83514.finance_profit_ledger l 
+  where asin is not null
+  ) using(sku)
+)
 
-
-union all
-
-select 
-distinct asin as channel_product_id, 
-category as project_name,
-category,
-last_updated::datetime as creation_date
-from datahawk_writable_83514.brandhut.manual_product_categories
-left join (
-select distinct sku, asin 
-from datahawk_share_83514.custom_83514.finance_profit_ledger l 
-where asin is not null
-) using(sku)
-
+SELECT * from pre_dedupe
+WHERE category NOT IN ('ZENS','ONANOFF','Zens BH','roku','CLA','Zens Qi2','OMG','EUCONIC SUPPLEMENT LIFE')
 order by channel_product_id,creation_date
