1a2fc3599eea089342d449f188207bb948a20171
aaronbunda1 <aaronbunda@gmail.com>
2025-01-02 17:26:00 -0500

    manual updates (#75)

--- bh_dbt/macros/brandhut_logic.sql
+++ bh_dbt/macros/brandhut_logic.sql
@@ -5,14 +5,14 @@
         when {{ sku_column }} ilike '%ZED%' or {{ sku_column }} in (
             'ZEAPM03/00',
             'ZESC08W') 
-            or {{ sku_column }} like 'ZE%' then 'ZENS'
+            or {{ sku_column }} ilike 'ZE%' then 'ZENS'
         when {{sku_column}} ilike '%zens%' then 'ZENS'
         when {{ sku_column }} ilike any ('%storyph%','%ss-%') then 'Onanoff 2'
         when {{ sku_column }} ilike any ('%fokus%') then 'Fokus'
         when {{ sku_column }} ilike '%SPOT%' then 'SPOT'
         when {{ sku_column }} ilike '%QI%' then 'Qisten'
         when {{ sku_column }} ilike any ('CL%','%cellini%') then 'Cellini'
-        when {{ sku_column }} like any ('%pop-fun%','%BP-%','BP-%','%-ON-%','%ON-%','%buddy%','%onanoff%','%onanoff%','%on-%','%bp-%','%play%','%fun%','%school%','%onanff%','%Onaonff%','%cosmo%','%explore%','%buddy%','%phones%') then 'ONANOFF'
+        when {{ sku_column }} ilike any ('%pop-fun%','%BP-%','BP-%','%-ON-%','%ON-%','%buddy%','%onanoff%','%onanoff%','%on-%','%bp-%','%play%','%fun%','%school%','%onanff%','%Onaonff%','%cosmo%','%explore%','%buddy%','%phones%') then 'ONANOFF'
         when {{ sku_column }} IN ('1005-30oz-SB') then 'Legacy'
     end
 {% endmacro %}
