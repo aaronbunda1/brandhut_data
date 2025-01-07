34ff69894ba9ee093b2c47e6ba5fe8c578cd7628
aaronbunda1 <aaronbunda@gmail.com>
2025-01-07 11:38:42 -0500

    Update brandhut_logic.sql

--- bh_dbt/macros/brandhut_logic.sql
+++ bh_dbt/macros/brandhut_logic.sql
@@ -1,10 +1,10 @@
 
 {% macro get_brand_from_sku(sku_column) %}
     case  
-        when {{ sku_column }} ilike '%alora%' then 'Alora'
         when {{ sku_column }} ilike '%shield%' then 'Onanoff 2'
         when {{ sku_column }} ilike any ('%roku%','%sunny%','%siri%') then '73&Sunny'
         when {{ sku_column }} ilike any ('%-TH-%') then 'Tiny Tree Houses'
+        when {{ sku_column }} ilike '%alora%' then 'Alora'
         when {{ sku_column }} ilike '%ZED%' or {{ sku_column }} in (
             'ZEAPM03/00',
             'ZESC08W') 
