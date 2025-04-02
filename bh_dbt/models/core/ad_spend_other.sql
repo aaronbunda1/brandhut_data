7db5f12e0965a79380645ce048f7ad33a65c511c
aaronbunda1 <aaronbunda@gmail.com>
2025-04-02 19:32:49 -0400

    Update ad_spend_other.sql

--- bh_dbt/models/core/ad_spend_other.sql
+++ bh_dbt/models/core/ad_spend_other.sql
@@ -6,7 +6,7 @@ date_trunc(month,date_day) as date_day,
 sum(case when sponsored_type = 'SponsoredBrands' then costs else 0 end) as sponsored_brands_cost,
 sum(case when sponsored_type = 'SponsoredDisplay' then costs else 0 end) as sponsored_display_cost
 from datahawk_share_83514.advertising.advertising_campaign_metrics acm
-where account_key = 'A6XVUKTIG2TMF'
+where account_key  IN ('A6XVUKTIG2TMF','A2QZG9U5PYAA59')
 and marketplace_key IS NOT NULL 
 group by all
 
